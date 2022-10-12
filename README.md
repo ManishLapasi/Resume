9808e9573061cdf7e5aab2260c148e6379595c3e
Manish Lapasi <mlp7@gatech.edu>
2022-10-12 00:39:47 -0400

    infra provisioning and deployment v1

---
+++ README.md
@@ -0,0 +1,6 @@
+# Set up
+
+1. Run `terraform apply` from the `terraform` directory
+2. Pre-requisites : 
+    - Need a service account with access to docker registry gcr.io
+    - Save this to `key.json` file in the `terraform` directory
--- terraform/main.tf
+++ terraform/main.tf
@@ -21,21 +21,42 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "g1-small"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only"
+    ]
   }
 }
 
 resource "null_resource" "authSetup" {
+  depends_on = [google_container_cluster.primary, google_container_node_pool.primary_preemptible_nodes]
   provisioner "local-exec" {
-    command = "gcloud container clusters get-credentials my-gke-cluster"
+    command = "gcloud container clusters get-credentials my-gke-cluster --zone us-central1-a"
   }
 }
 
-resource "kubectl_manifest" "pod_deployment" {
+resource "null_resource" "secretSetup" {
   depends_on = [null_resource.authSetup]
+  provisioner "local-exec" {
+    command = "kubectl create secret docker-registry my-secret --from-file=.dockerconfigjson=key.json"
+  }
+}
+
+provider "kubernetes" {
+  load_config_file = "false"
+  host     = google_container_cluster.primary.endpoint
+  client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+  client_key             = google_container_cluster.primary.master_auth.0.client_key
+  cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+}
+
+resource "kubectl_manifest" "pod_deployment" {
+  depends_on = [null_resource.secretSetup]
   yaml_body = file("../kubernetes/node-deployment.yml")
 }
 
 resource "kubectl_manifest" "service_deployment" {
-  depends_on = [null_resource.authSetup]
+  depends_on = [null_resource.secretSetup]
   yaml_body = file("../kubernetes/node-service.yml")
 }
